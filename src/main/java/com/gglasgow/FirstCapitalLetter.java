81752c2b20f826d8080c61cf59e64e2b8f47eca9
Gerald Glasgow <geraldjglasgow@gmail.com>
2024-10-10 00:42:59 -0400

    adding FirstCapitalLetter recursive solution

---
+++ src/main/java/com/gglasgow/FirstCapitalLetter.java
@@ -0,0 +1,27 @@
+package com.gglasgow;
+
+public class FirstCapitalLetter {
+    public static void main(String[] args) {
+        // Input : thequickbrownfOx
+        // Output : K
+        // Given a string find its first uppercase letter 
+        String str = "thequickbrownFox";
+        char res = first(str,0);
+        if (res == 0)
+            System.out.println("No uppercase letter");
+        else
+            System.out.println (res);
+    }
+
+    private static char first(String str, int index) {
+        if (index >= str.length()) {
+            return 0;
+        }
+        if (Character.isUpperCase(str.charAt(index))) {
+            return str.charAt(index);
+        }
+        index++;
+        return first(str, index);
+    }
+
+}
